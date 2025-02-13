40e684971f2399cffa504a2c0d71646f946222b2
trueToastedCode <lennard2hp@gmail.com>
2025-02-13 19:15:16 +0100

    change default host

--- healthcheck_server.py
+++ healthcheck_server.py
@@ -1,5 +1,5 @@
 def make_health_check_server(web):
-    async def health_check_server(get_is_healthy, port=3000):
+    async def health_check_server(get_is_healthy, host='0.0.0.0', port=3000):
         async def health_check(request):
             if request.path == '/healthcheck':
                 return web.Response(status=200 if get_is_healthy() else 500)
@@ -11,7 +11,7 @@ def make_health_check_server(web):
         runner = web.AppRunner(app)
         await runner.setup()
 
-        site = web.TCPSite(runner, 'localhost', port)
+        site = web.TCPSite(runner, host, port)
         await site.start()
 
     return health_check_server
